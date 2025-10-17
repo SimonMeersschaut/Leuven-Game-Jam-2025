8bfb3acacda3db9f07825db2899fdb4b844b9c4b
Tijn Somers <tijn.somers@student.kuleuven.be>
2025-10-17 19:19:36 +0200

    add button to engine

--- menu/menu.py
+++ menu/menu.py
@@ -1,40 +1,34 @@
 from engine import Engine, engine, Modes
+from engine import button as btn
 import pygame
 
 class Menu:
     def __init__(self):
         self.state = "main_menu"
-    
+        self.buttons = []
+        
+        self.play_button = btn.Button('resources/buttons/play_paper.jpeg', (400, 160), height=100)
+        self.credits_button = btn.Button('resources/buttons/credits_paper.jpeg', (400, 300), height=100)
+
     def update(self, delta_t, events):
-        for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.state == "main_menu":
-                    if pygame.mouse.get_pos()[0] in range(400, 600) and pygame.mouse.get_pos()[1] in range(160, 260):
-                        engine.mode = Modes.game
-                    elif pygame.mouse.get_pos()[0] in range(400, 600) and pygame.mouse.get_pos()[1] in range(300, 400):
-                        self.state = "credits"
-                elif self.state == "credits":
-                    if pygame.mouse.get_pos()[0] in range(400, 600) and pygame.mouse.get_pos()[1] in range(400, 500):
-                        self.state = "main_menu"
+        if self.play_button.is_clicked():
+            engine.mode = Modes.game
+        elif self.credits_button.is_clicked():
+            self.state = "credits"
 
     def render(self):
         engine.fill((60, 60, 60))
         
         paper_background = pygame.transform.scale(engine.get_image('resources/buttons/paper_background.jpeg'), (1000, 600))
         engine.render_image(paper_background, (0, 0))
-        
-        
+
+        self.play_button.render()
+        self.credits_button.render()
 
         if self.state == "main_menu":
             title = engine.render_text("pixel", 48, "KUL Game Jam 2025", (255, 255, 255))
             engine.render_image(title, (250, 20))
-            
-            play_button = pygame.transform.scale(engine.get_image('resources/buttons/play_paper.jpeg'), (200, 100))
-            engine.render_image(play_button, (400, 160))
-
-            credits_button = pygame.transform.scale(engine.get_image('resources/buttons/credits_paper.jpeg'), (200, 100))
-            engine.render_image(credits_button, (400, 300))
-
+        
         elif self.state == "credits":
             
             credits_text = pygame.transform.scale(engine.get_image('resources/buttons/credits_list_paper.png'), (2448/6, 3262/6))
