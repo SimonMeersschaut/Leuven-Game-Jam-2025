09dc1d11da36fb361ab2528f8de24b9d2b85f03f
Tijn Somers <tijn.somers@student.kuleuven.be>
2025-10-18 12:02:05 +0200

    update credits text to new system

--- menu/credits.py
+++ menu/credits.py
@@ -1,12 +1,15 @@
 from engine import Engine, engine, Modes
-from engine import sprite as btn
+from engine.sprite import Button, Sprite
 import pygame
 
 class Credits:
     def __init__(self, main_menu):
         self.state = "main_menu"
-        self.back_button = btn.Button('resources/buttons/back_paper.jpeg', (400, 400), height=100)
+        self.back_button = Button('resources/buttons/back_paper.jpeg', (-1, 400), height=100, align_x="center")
         self.main_menu = main_menu
+        
+        self.credits_text = Sprite('resources/buttons/credits_list_paper.png', (-1, 0), 2448/6, align_x="center")
+
 
     def update(self, delta_t, events):
         if self.back_button.is_clicked():
@@ -20,6 +23,5 @@ class Credits:
         engine.render_image(paper_background, (0, 0))
 
         self.back_button.render()
-
-        credits_text = pygame.transform.scale(engine.get_image('resources/buttons/credits_list_paper.png'), (2448/6, 3262/6))
-        engine.render_image(credits_text, (500 - credits_text.get_width()/2, -100))
+        
+        self.credits_text.render()
