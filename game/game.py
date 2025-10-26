dae20cf8b33e61f29d8331208dcee10adafa11d2
Robbe Piessens <robbe.piessens@gmail.com>
2025-10-26 17:14:02 +0100

    golden poop

--- game/game.py
+++ game/game.py
@@ -7,14 +7,16 @@ from .plate.plate_supervisor import PlateSupervisor
 from .stats import Stats
 from .loading_bar import Loadingbar
 from .game_over_screen import Gameoverscreen
+from .golden_poop import Goldenpoop
 
 class Game:
     def __init__(self):
         self.wave_number = 0        
         # self.hond = Hond()
         self.slang = Snake()
+        self.floor_image=pygame.transform.scale(engine.get_image('resources/images/Floor.png'),(engine.DISPLAY_W,0.42*engine.DISPLAY_H))
         self.repeating_cupboard=3
-        self.cupboard_game_background=pygame.transform.scale(engine.get_image('resources/images/cupboard.png'),(engine.DISPLAY_W/self.repeating_cupboard,0.75*engine.DISPLAY_H))
+        self.cupboard_game_background=pygame.transform.scale(engine.get_image('resources/images/cupboard.png'),(engine.DISPLAY_W/self.repeating_cupboard,0.6*engine.DISPLAY_H))
         self.elephant_ass=engine.get_image("resources/images/elephant_ass.png")
         self.elephant_ass=pygame.transform.scale_by(self.elephant_ass,0.2)
         self.width_elephant_ass,self.length_elephant_ass=self.elephant_ass.get_size()
@@ -23,23 +25,26 @@ class Game:
         self.gameoverscreen = Gameoverscreen()
         self.plate_supervisor = PlateSupervisor(self, self.loading_bar, self.stats)
         self.plate_supervisor.spawn_plate()
+        self.golden_poop = Goldenpoop()
         
 
     def update(self, delta_t: float, events: list):
         # self.hond.update(delta_t, events)
-        self.slang.update(delta_t,events,self.loading_bar,self.stats)
+        self.slang.update(delta_t,events,self.loading_bar,self.stats,self.golden_poop)
         
         self.plate_supervisor.update(delta_t, events)
         
         self.stats.update(delta_t,events)
         self.loading_bar.update(delta_t,events)
         self.gameoverscreen.update(delta_t,events,self.stats,self.loading_bar)
+        self.golden_poop.update(delta_t,events)
 
     def render(self):
         engine.fill((0, 0, 0))
         # self.hond.render()
         # self.slang.render()
         self.plate_supervisor.prerender()
+        engine.render_image(self.floor_image,(0,engine.DISPLAY_H*0.58))
         for cupboard_nr in range(self.repeating_cupboard):
             engine.render_image(self.cupboard_game_background,(cupboard_nr*engine.DISPLAY_W/self.repeating_cupboard,0))
         engine.render_image(self.elephant_ass,(engine.DISPLAY_W/2-self.width_elephant_ass/2,engine.DISPLAY_H-self.length_elephant_ass-50))
@@ -49,6 +54,7 @@ class Game:
         if self.stats.lives <=0:
             self.gameoverscreen.render()
         self.slang.render()
+        self.golden_poop.render()
 
     
 game = Game()
---
+++ game/golden_poop.py
@@ -0,0 +1,37 @@
+from engine import engine
+from engine import Engine, engine, Modes
+from engine.sprite import Button, Sprite
+import pygame
+
+class Goldenpoop:
+    def __init__(self):
+        self.golden_poop_button=Button('resources/buttons/golden_poop.png', (engine.DISPLAY_W/2, 160), height=100, align_x="center")
+        self.speed=500
+        self.golden_poop=False
+        self.y_goldenpoop=0
+        self.captured=False
+
+
+    def update(self, delta_t: float, events: list):
+        if self.golden_poop:
+            self.y_goldenpoop+=self.speed*delta_t
+            self.golden_poop_button.move(engine.DISPLAY_W/2,self.y_goldenpoop)
+            if self.golden_poop_button.update_and_check_clicked():
+                self.captured=True
+            if self.y_goldenpoop>engine.DISPLAY_H:
+                self.golden_poop=False            
+
+    def golden_poop_appears(self):
+        self.x_goldenpoop=engine.DISPLAY_W/2
+        self.y_goldenpoop=engine.DISPLAY_H/2
+        self.golden_poop=True
+
+    def render(self):
+        if self.golden_poop:
+            self.golden_poop_button.render()
+
+
+        
+
+
+
