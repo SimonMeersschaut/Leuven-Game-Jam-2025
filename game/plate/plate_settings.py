bf0f8be5813b8aa99fe5c8a5193d467620d1afa1
Tijn Somers <tijn.somers@student.kuleuven.be>
2025-10-26 16:49:37 +0100

    added more colors

--- game/plate/plate_settings.py
+++ game/plate/plate_settings.py
@@ -21,11 +21,46 @@ PLATE_IMAGES = [
         "symbol": "flower",
         "image": "flower_red.png"
     },
+    {
+        "color": "purple",
+        "symbol": "flower",
+        "image": "flower_purple.png"
+    },
+    {
+        "color": "cyan",
+        "symbol": "flower",
+        "image": "flower_cyan.png"
+    },
     # Bird plates
     {
         "color": "blue",
         "symbol": "bird",
         "image": "bird_blue.png"
+    },
+    {
+        "color": "red",
+        "symbol": "bird",
+        "image": "bird_red.png"
+    },
+    {
+        "color": "green",
+        "symbol": "bird",
+        "image": "bird_green.png"
+    },
+    {
+        "color": "black",
+        "symbol": "bird",
+        "image": "bird_black.png"
+    },
+    {
+        "color": "purple",
+        "symbol": "bird",
+        "image": "bird_purple.png"
+    },
+    {
+        "color": "cyan",
+        "symbol": "bird",
+        "image": "bird_cyan.png"
     },
 
     # Fish plates
@@ -34,6 +69,31 @@ PLATE_IMAGES = [
         "symbol": "fish",
         "image": "fish_blue.png"
     },
+    {
+        "color": "red",
+        "symbol": "fish",
+        "image": "fish_red.png"
+    },
+    {
+        "color": "green",
+        "symbol": "fish",
+        "image": "fish_green.png"
+    },
+    {
+        "color": "black",
+        "symbol": "fish",
+        "image": "fish_black.png"
+    },
+    {
+        "color": "purple",
+        "symbol": "fish",
+        "image": "fish_purple.png"
+    },
+    {
+        "color": "cyan",
+        "symbol": "fish",
+        "image": "fish_cyan.png"
+    }
 ]
 
 # define the price of each colored plate (per eighth of a plate)
